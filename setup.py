d094dec0d373e0381ac0e693233d297236e7effd
Kyle Cirillo <54098205+kyle3113@users.noreply.github.com>
2020-10-13 09:39:49 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from disutils.core import setup
+
+setuptools.setup(
+    name="network-kyle3113", # Replace with your own username
+    version="0.0.1",
+    author="Deeplab",
+    author_email="author@example.com",
+    description="A small example package",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/kyle3113/DeepLabV3Plus-Pytorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
